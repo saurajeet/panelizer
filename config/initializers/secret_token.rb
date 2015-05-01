8ce673786e8e373b83bdc14c58ba59e49a14111e
Saurajeet Dutta <saurajeet.d@directi.com>
2015-05-01 08:50:02 +0530

    +Skel

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Panelizer::Application.config.secret_key_base = '3cc6da3caf75e1ffb11b0a4fd24106fb52cb7676ac8b9aeb3115c48fff19655da4e0f2456fa67f22092f06437de283414a5b832070c4bd3fd80b397a8692df89'
